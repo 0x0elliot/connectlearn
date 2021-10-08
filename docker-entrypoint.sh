ba661f616597cf0ec0181b1d64f05d8fc5e53391
elliot <macuser@Macs-MacBook-Air.local>
2021-10-09 04:44:32 +0530

    Adding netlify addition

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 echo "Starting ConnectLearn"
+
+port="8000"
+
+if [[ "$environment" == "netlify" ]]; then
+	echo "Deployed on netlify"
+	port="443"
+fi
+
+echo "$port"
+
 exec gunicorn 'src:app' \
-    --bind '0.0.0.0:8000' \
+    --bind "0.0.0.0:${port}" \
     --workers 5 \
